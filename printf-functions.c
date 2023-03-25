728713c08e5607f6bff2cd8446afcb843514eaaa
Kyuka Avong <drkyavoni@live.com>
2023-03-25 08:09:19 +0100

    updated again

--- printf-functions.c
+++ printf-functions.c
@@ -5,10 +5,19 @@
 #include <stddef.h>
 #include <stdarg.h>
 
+/* function pointer for _printf */
+/* s is a string pointer that represents the string */
+/* arg is a token string */
+/* for example _printf("do something %c", var1) */
+/* c is of type dt.c */
+/* var1 is of type dt.t */
 void (*f)(char *s, dt *args)
 {
     /* if s is empty, return an exit */
     if (s == NULL)
         exit(1);
-    
+    /* break down the s*/
+    /* get the delimiters starting with %*/
+    /* map var1 with the type dt.t */
+    /* write the result to stdout */
 }
