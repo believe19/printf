0e7b0f7cafb4a40d7da61181631293dd6fc8c59a
Kyuka Avong <drkyavoni@live.com>
2023-03-25 07:38:42 +0100

    updated

---
+++ main.h
@@ -0,0 +1,16 @@
+/* printf header - main.h */
+
+#ifndef PRINTF_CALL_H
+#define PRINTF_CALL_H
+
+/* create a prototype for printf */
+void (*f)(char *c, *args);
+
+/* create a struct to host the repository */
+typedef struct datatype 
+{
+	char c;
+	typeof t;
+} dt;
+
+#endif /* PRINTF_CALL_H */
